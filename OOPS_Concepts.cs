954e28a41f9dc4797c5b6b7d39818d4d635691d0
rajkumar9755 <46595286+rajkumar9755@users.noreply.github.com>
2019-01-11 17:28:26 +0530

    Create OOPS_Concepts.cs

---
+++ OOPS_Concepts.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace OOPS_Concepts{
+   
+   abstract class Shape{
+      public abstract int getArea();
+   }
+   class Rectangle:Shape {
+       
+       protected int height,width;/* These data members can't be accessable in Solution class*/
+       
+       /*Line No 16-25 is example for Compile time polymorphism*/
+       public Rectangle()
+       {
+           width=1;
+           height=1;
+       }
+       public Rectangle(int h,int w)
+       {
+           width=w;
+           height=h;
+       }
+       /*Overriding of getArea() method is example for Runtime polmorphism*/
+       public override int getArea()
+        {
+           return width*height;
+        }
+   }
+   class Square:Rectangle{
+       public Square()
+       {
+           width=1;
+           height=1;
+       }
+       public Square(int side)
+       {
+           width=side;
+           height=side;
+       }
+       
+   }
+   class Circle:Shape{
+       private int radius;
+       public Circle()
+       {
+           radius=1;
+       }
+       public Circle(int rad)
+       {
+           radius=rad;
+       }
+       /*Below method is example for Runtime Polymorphism*/
+       public override int getArea()
+        {
+           return (22/7)*radius*radius;
+        }
+   }
+   class Solution {
+      static void Main(string[] args) {
+         Rectangle rect1=new Rectangle();
+         Rectangle rect2= new Rectangle(4,5);
+         Square sqr=new Square(4);
+         Circle cir=new Circle(5);
+         Console.WriteLine("Rectangle1 Area: {0}",  rect1.getArea());
+         Console.WriteLine("Rectangle2 Area: {0}",  rect2.getArea());
+         Console.WriteLine("Square Area: {0}",  sqr.getArea());
+         Console.WriteLine("Circle Area: {0}",  cir.getArea());
+         Console.ReadKey();
+      }
+   }
+}
